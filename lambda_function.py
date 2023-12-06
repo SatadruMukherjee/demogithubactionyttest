b357eb63610cd8db268c12d890186e508826da56
SatadruMukherjee <48749591+SatadruMukherjee@users.noreply.github.com>
2023-12-06 13:33:49 +0530

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,28 @@
+import snowflake.connector as sf
+
+def run_query(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    cursor.close()
+
+def lambda_handler(event, context):
+    user="Satadru"
+    password="Sattu12345@"
+    account="iy56276.us-east-2.aws";
+    database="RAMU"
+    warehouse="COMPUTE_WH"
+    schema="PUBLIC"
+    role="ACCOUNTADMIN"
+    conn=sf.connect(user=user,password=password,account=account);
+
+
+    statement_1='use warehouse '+warehouse;
+    statement3="use database "+database;
+    statement4="use role "+role;
+    run_query(conn,statement_1)
+    run_query(conn,statement3)
+    run_query(conn,statement4)
+    sql_query_table_creation = "CREATE OR REPLACE TABLE test_table(col1 integer, col2 string)"
+    run_query(conn, sql_query_table_creation);
+    sql_query_data_ingestion="INSERT INTO test_table(col1, col2) VALUES    (123, 'test string1'), (456, 'test string2')"
+    run_query(conn, sql_query_data_ingestion);
