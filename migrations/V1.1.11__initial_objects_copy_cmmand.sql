254f0d008ab1eca4332f8e9ce25573c98da08064
SatadruMukherjee <48749591+SatadruMukherjee@users.noreply.github.com>
2023-12-07 23:11:34 +0530

    Add files via upload

---
+++ migrations/V1.1.11__initial_objects_copy_cmmand.sql
@@ -0,0 +1,17 @@
+create or replace table demoytcicd.public.sample_test123 (ChartIdentifier varchar(200) ,PdfFileName varchar(200) ,chase_dxclass varchar(1000));
+
+--create the file format
+create or replace file format demoytcicd.public.my_csv_format
+type = csv field_delimiter = ',' skip_header = 1
+field_optionally_enclosed_by = '"'
+null_if = ('NULL', 'null') 
+empty_field_as_null = true;
+
+--create the external stage
+create or replace stage demoytcicd.public.Snow_stage url="s3://normaldatapoint/" 
+credentials=(aws_key_id='AKIA4ASLMUJLT4BUQD3B'
+aws_secret_key='VDDcs71tVAjyqvcrE6cUJnf0Hfo/Ikis0KsPQ58y')
+file_format = my_csv_format;
+
+copy into demoytcicd.public.sample_test123 from 
+@demoytcicd.PUBLIC.Snow_stage FILE_FORMAT=(FORMAT_NAME=my_csv_format);
